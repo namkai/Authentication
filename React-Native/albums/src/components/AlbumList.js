3e0418fcd6c8bd12a6da24a82823ff8dc0ffea2f
Namkai Fairfield <namkai.fairfield@gmail.com>
2017-02-24 13:26:13 -0800

    first commit to albums

---
+++ React-Native/albums/src/components/AlbumList.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
+
+
+class AlbumList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            albums: []
+        };
+    }
+    componentWillMount() {
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums').then(response =>
+            this.setState({ albums: response.data }));
+    }
+    renderAlbums() {
+        const { albums } = this.state;
+
+        return albums.map((album) => (
+                <AlbumDetail key={album.title} album={album} />
+            ));
+    }
+    render() {
+        if (this.state.albums.length === 0) {
+            return (
+                <View>
+                    <Text>Loading....</Text>
+                </View>
+            );
+        }
+        return (
+            <View>
+                {this.renderAlbums()}
+            </View>
+        );
+    }
+}
+
+export default AlbumList;
